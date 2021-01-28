e6ecd269aff09163a258fa5d195bef541943390d
jerry <jerryli720@qq.com>
2021-01-28 14:58:24 +0800

    chore: bump version 2.1.3

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -6,6 +6,12 @@ module.exports = {
     node: true,
     es6: true,
   },
+  parserOptions: {
+    ecmaVersion: 6,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   extends: [
     '@react-native-community',
     'airbnb',
