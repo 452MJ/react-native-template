8e261bad1d18283b4ca3f7d4a1fd69228bc99b1a
jerry <jerryli720@qq.com>
2021-08-26 16:34:50 +0800

    chore: bump version 2.2.3

--- template/ios/Awesome/AppDelegate.m
+++ template/ios/Awesome/AppDelegate.m
@@ -6,6 +6,10 @@
 #import "Orientation.h"
 #import <CodePush/CodePush.h>
 
+#import <AppCenterReactNative.h>
+#import <AppCenterReactNativeAnalytics.h>
+#import <AppCenterReactNativeCrashes.h>
+
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
 #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
@@ -32,6 +36,10 @@ static void InitializeFlipper(UIApplication *application) {
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
+
+  [AppCenterReactNative register];
+  [AppCenterReactNativeAnalytics registerWithInitiallyEnabled:true];
+  [AppCenterReactNativeCrashes registerWithAutomaticProcessing];
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
--- template/src/entry.jsx
+++ template/src/entry.jsx
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler'
-import React, { Component } from 'react'
+import React, { Component, useEffect } from 'react'
 import { Provider } from '@ant-design/react-native'
-import { StatusBar } from 'react-native'
+import { Platform, StatusBar } from 'react-native'
 import { setColor } from 'react-native-navbar-color'
 import GlobalNavigation from './utils/GlobalNavigation'
 import LoadingIndicator from './components/LoadingIndicator'
@@ -23,74 +23,76 @@ global.$storage = storages
 global.$http = http
 global.$i18n = i18nUtil
 
-class Entry extends Component {
-  async componentWillMount() {
-    if (Platform.OS === 'android') {
-      setColor('#000000')
-    }
-
-    const userInfo = (await $storage.getData($storage.KEYS.userInfo)) || {}
-    if (userInfo) {
-      $store.dispatch({ type: 'user/updateUserInfo', payload: userInfo })
-    }
-
-    await this.initLocale()
-  }
-
-  initLocale = async () => {
-    const defaultLocale = {
-      isRTL: false,
-      languageTag: 'zh-TW',
-      countryCode: 'TW',
-      languageCode: 'zh',
-    }
-    // 语言
-    const locale =
-      (await $storage.getData($storage.KEYS.locale)) || defaultLocale
-
-    $i18n.setI18nConfig(locale)
-    $store.dispatch({ type: 'settings/setLocale', payload: locale })
-  }
-
-  render() {
-    return (
-      <Provider>
-        <StatusBar
-          animated
-          translucent
-          // barStyle="light-content"
-          // backgroundColor="rgba(0,0,0,0)"
-        />
-        <Router />
-
-        <LoadingIndicator
-          ref={ref => {
-            global.$loading = ref
-          }}
-        />
-        <CustomToast
-          ref={ref => {
-            global.$toast = {
-              info: msg => {
-                ref.info(msg)
-              },
-              success: msg => {
-                ref.success(msg)
-              },
-              error: msg => {
-                ref.error(msg)
-              },
-            }
-          }}
-        />
-        <ModalCodePush
-          ref={ref => {
-            global.$codepush = ref
-          }}
-        />
-      </Provider>
-    )
-  }
-}
-
 export default Entry
+function Entry() {
+  useEffect(() => {
+    const init = async () => {
+      if (Platform.OS === 'android') {
+        setColor('#000000')
+      }
+
+      const userInfo = (await $storage.getData($storage.KEYS.userInfo)) || {}
+      if (userInfo) {
+        $store.dispatch({ type: 'user/updateUserInfo', payload: userInfo })
+      }
+
+      await initLocale()
+    }
+
+    const initLocale = async () => {
+      const defaultLocale = {
+        isRTL: false,
+        languageTag: 'zh-TW',
+        countryCode: 'TW',
+        languageCode: 'zh',
+      }
+      // 语言
+      const locale =
+        (await $storage.getData($storage.KEYS.locale)) || defaultLocale
+
+      $i18n.setI18nConfig(locale)
+      $store.dispatch({ type: 'settings/setLocale', payload: locale })
+    }
+
+    console.log(1)
+    init()
+  }, [])
+
+  return (
+    <Provider>
+      <StatusBar
+        animated
+        translucent
+        // barStyle="light-content"
+        // backgroundColor="rgba(0,0,0,0)"
+      />
+      <Router />
+
+      <LoadingIndicator
+        ref={ref => {
+          global.$loading = ref
+        }}
+      />
+      <CustomToast
+        ref={ref => {
+          global.$toast = {
+            info: msg => {
+              ref.info(msg)
+            },
+            success: msg => {
+              ref.success(msg)
+            },
+            error: msg => {
+              ref.error(msg)
+            },
+          }
+        }}
+      />
+      <ModalCodePush
+        ref={ref => {
+          global.$codepush = ref
+        }}
+      />
+    </Provider>
+  )
+}
