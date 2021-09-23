7c7710f896f74d6249d203e29a96286da428e5a0
jerry <jerryli720@qq.com>
2021-09-23 15:26:38 +0800

    chore: bump version 2.4.0

--- template/android/app/src/main/java/com/awesome/MainApplication.java
+++ template/android/app/src/main/java/com/awesome/MainApplication.java
@@ -13,8 +13,10 @@ import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JSIModulePackage;
 import com.facebook.soloader.SoLoader;
 import com.microsoft.codepush.react.CodePush;
+import com.swmansion.reanimated.ReanimatedJSIModulePackage;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
@@ -36,6 +38,12 @@ public class MainApplication extends Application implements ReactApplication {
                     // packages.add(new MyReactNativePackage());
                     return packages;
                 }
+
+
+              @Override
+              protected JSIModulePackage getJSIModulePackage() {
+                return new ReanimatedJSIModulePackage(); // <- add
+              }
 
                 @Override
                 protected String getJSMainModuleName() {
