f9f5c6b7795399d2ac4c2e0e7a3ce6a62d34f12f
jerry <jerryli720@qq.com>
2021-11-25 11:47:45 +0800

    chore: bump version 2.5.0

--- template/src/components/ModalCodePush.jsx
+++ template/src/components/ModalCodePush.jsx
@@ -37,18 +37,42 @@ export default class ModalCodePush extends React.PureComponent {
       const currentPatch = await codePush.getUpdateMetadata()
       const latestPatch = await codePush.checkForUpdate()
 
-      // console.log('codepush', currentPatch, latestPatch)
       $loading.hide()
       if (latestPatch === null) {
         // 无更新包
-        if (showTips) $toast.show('It is currently the latest version.')
+        if (showTips)
+          $toast.show(
+              $i18n.translation(
+                  `当前已是最新版本`,
+                  `當前已是最新版本`,
+                  `It is currently the latest version.`
+              )
+          )
       } else {
-        this.setState({ patch: latestPatch, visible: true })
+        const isSilent = latestPatch.description?.indexOf('silent') === 0
+
+        this.setState(
+            {
+              patch: latestPatch,
+              visible: isSilent === false,
+            },
+            () => {
+              if (isSilent) {
+                this.startDownload(true)
+              }
+            }
+        )
       }
     } catch (e) {
       $loading.hide()
       if (showTips) {
-        $toast.show(`Failed to check the new version.`)
+        $toast.show(
+            $i18n.translation(
+                `检测新版本失败`,
+                `檢測新版本失敗`,
+                `Failed to check the new version.`
+            )
+        )
       }
     }
   }
@@ -56,142 +80,142 @@ export default class ModalCodePush extends React.PureComponent {
   render = () => {
     const { description = null, label = null } = this.state.patch
     return (
-      <Modal
-        isVisible={this.state.visible}
-        style={{
-          margin: 0,
-          padding: 0,
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-        useNativeDriver
-      >
-        <View
-          style={{
-            top: apx(-1),
-            width: apx(560),
-            alignItems: 'center',
-            backgroundColor: '#fff',
-            borderRadius: apx(20),
-          }}
+        <Modal
+            isVisible={this.state.visible}
+            style={{
+              margin: 0,
+              padding: 0,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+            useNativeDriver
         >
-          <Text
-            style={{
-              fontSize: apx(36),
-              fontWeight: '500',
-              marginTop: apx(20),
-              color: '#FEBE18',
-            }}
+          <View
+              style={{
+                top: apx(-1),
+                width: apx(560),
+                alignItems: 'center',
+                backgroundColor: '#fff',
+                borderRadius: apx(20),
+              }}
           >
-            发现新版本
-          </Text>
-          <Text
-            style={{
-              fontSize: apx(22),
-              marginVertical: apx(20),
-              color: '#323F4B',
-            }}
-          >
-            新版本 {DeviceInfo.getVersion()} ({label})
-          </Text>
-
-          {this.state.downloading ? (
-            <View style={{ marginBottom: apx(48) }}>
-              <Text
+            <Text
                 style={{
-                  textAlign: 'center',
-                  marginBottom: apx(20),
-                  fontSize: apx(26),
-                  color: '#617485',
+                  fontSize: apx(36),
+                  fontWeight: '500',
+                  marginTop: apx(20),
+                  color: '#FEBE18',
                 }}
-              >
-                {this.state.current}MB / {this.state.total}MB
-              </Text>
-              <ProgressBar
-                progress={this.state.progress}
-                activeColor="#FEBE18"
-                width={apx(452)}
-                height={apx(20)}
-              />
-            </View>
-          ) : (
-            <View style={{ alignItems: 'center' }}>
-              <Button
-                text="立即更新"
-                theme="gold"
-                contentContainerStyle={{ width: apx(452), height: apx(80) }}
-                textStyle={{ fontSize: apx(26) }}
-                onPress={this.startDownload}
-              />
-
-              <Touchable
+            >
+              发现新版本
+            </Text>
+            <Text
                 style={{
-                  marginBottom: apx(18),
+                  fontSize: apx(22),
+                  marginVertical: apx(20),
+                  color: '#323F4B',
                 }}
-                contentContainerStyle={{
-                  width: apx(452),
-                  paddingVertical: apx(30),
-                  alignItems: 'center',
-                }}
-                onPress={() => this.setState({ visible: false })}
-              >
-                <Text
-                  style={{
-                    fontSize: apx(26),
-                    color: '#FEBE18',
-                    textAlign: 'center',
-                  }}
-                >
-                  稍后更新
-                </Text>
-              </Touchable>
-            </View>
-          )}
-        </View>
-      </Modal>
+            >
+              新版本 {DeviceInfo.getVersion()} ({label})
+            </Text>
+
+            {this.state.downloading ? (
+                <View style={{ marginBottom: apx(48) }}>
+                  <Text
+                      style={{
+                        textAlign: 'center',
+                        marginBottom: apx(20),
+                        fontSize: apx(26),
+                        color: '#617485',
+                      }}
+                  >
+                    {this.state.current}MB / {this.state.total}MB
+                  </Text>
+                  <ProgressBar
+                      progress={this.state.progress}
+                      activeColor="#FEBE18"
+                      width={apx(452)}
+                      height={apx(20)}
+                  />
+                </View>
+            ) : (
+                <View style={{ alignItems: 'center', paddingBottom: apx(32) }}>
+                  <Button
+                      text="立即更新"
+                      theme="gold"
+                      contentContainerStyle={{ width: apx(452), height: apx(80) }}
+                      textStyle={{ fontSize: apx(26) }}
+                      onPress={this.startDownload}
+                  />
+
+                  {this.state.patch?.isMandatory === false && (
+                      <Touchable
+                          contentContainerStyle={{
+                            width: apx(452),
+                            paddingVertical: apx(30),
+                            alignItems: 'center',
+                          }}
+                          onPress={() => this.setState({ visible: false })}
+                      >
+                        <Text
+                            style={{
+                              fontSize: apx(26),
+                              color: '#FEBE18',
+                              textAlign: 'center',
+                            }}
+                        >
+                          稍后更新
+                        </Text>
+                      </Touchable>
+                  )}
+                </View>
+            )}
+          </View>
+        </Modal>
     )
   }
 
-  startDownload = async () => {
+  startDownload = async (silent = false) => {
+    !silent &&
     this.setState({
       downloading: true,
     })
 
-    if (Platform.OS === 'ios') {
-      await codePush.clearUpdates()
-      const latestPatch = await codePush.checkForUpdate()
-      this.state.patch = latestPatch
-    }
-
     const newPatch = await this.state.patch
-      .download(progress => {
-        this.setState({
-          current: (progress.receivedBytes / (1024 * 1024)).toFixed(2),
-          total: (progress.totalBytes / (1024 * 1024)).toFixed(2),
-          progress: progress.receivedBytes / progress.totalBytes,
-          downloading: true,
+        .download(progress => {
+          !silent &&
+          this.setState({
+            current: (progress.receivedBytes / (1024 * 1024)).toFixed(2),
+            total: (progress.totalBytes / (1024 * 1024)).toFixed(2),
+            progress: progress.receivedBytes / progress.totalBytes,
+            downloading: true,
+          })
         })
-      })
-      .catch(err => {
-        this.setState({
-          current: 0,
-          total: 0,
-          progress: 0,
-          visible: false,
-          downloading: false,
+        .catch(err => {
+          !silent &&
+          this.setState({
+            current: 0,
+            total: 0,
+            progress: 0,
+            visible: false,
+            downloading: false,
+          })
         })
-      })
 
     this.setState({ visible: false, downloading: false }, () => {
       newPatch
-        .install(codePush.InstallMode.IMMEDIATE)
-        .then(() => {
-          codePush.allowRestart()
-          codePush.restartApp()
-        })
-        .catch(err => {
-          console.log(err)
-        })
+          .install(
+              silent
+                  ? codePush.InstallMode.ON_NEXT_SUSPEND
+                  : codePush.InstallMode.IMMEDIATE
+          )
+          .then(() => {
+            codePush.allowRestart()
+            !silent && codePush.restartApp()
+          })
+          .catch(err => {
+            console.log(err)
+          })
     })
   }
 }
