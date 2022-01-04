f7ba2297ddcd00166999d2df3e22db22dd249693
jerry <jerryli720@qq.com>
2022-01-04 15:22:42 +0800

    chore: bump version 2.5.2 支持iOS高刷

--- template/ios/Awesome/AppDelegate.h
+++ template/ios/Awesome/AppDelegate.h
@@ -1,11 +1,10 @@
 #import <React/RCTBridgeDelegate.h>
 #import <UIKit/UIKit.h>
 
-API_AVAILABLE(ios(15.0))
 @interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate, UNUserNotificationCenterDelegate>
 
 @property (nonatomic, strong) UIWindow *window;
 
-@property(nonatomic) CAFrameRateRange preferredFrameRateRange;
+@property(nonatomic) CAFrameRateRange preferredFrameRateRange API_AVAILABLE(ios(15.0));
 
 @end
