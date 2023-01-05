47d260a8647f6dca61e41eb47de9ebe480a49599
jerry <jerryli720@qq.com>
2023-01-05 15:44:01 +0800

    chore: bump version 3.1.3

--- template/android/app/src/main/java/com/awesome/MainActivity.java
+++ template/android/app/src/main/java/com/awesome/MainActivity.java
@@ -6,6 +6,12 @@ import com.facebook.react.ReactRootView;
 
 public class MainActivity extends ReactActivity {
 
+   @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //react-native-screens override
+        super.onCreate(null);
+    }
+
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
